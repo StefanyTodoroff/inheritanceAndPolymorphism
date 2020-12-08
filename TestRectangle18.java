9bdde834fae289f804aa57725cb3156b4bca38d5
StefanyTodoroff <72222524+StefanyTodoroff@users.noreply.github.com>
2020-12-08 13:25:12 +0200

    Create TestRectangle18.java

---
+++ TestRectangle18.java
@@ -0,0 +1,16 @@
+package edu.smg;
+
+public class TestRectangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Rectangle rectangle = new Rectangle (2, 4);
+		System.out.println("\nA rectangle " + rectangle.toString());
+		System.out.println("The area is " + rectangle.getArea());
+		System.out.println("The perimeter is " + rectangle.getPerimeter());
+
+	}
+
+}
+ 
