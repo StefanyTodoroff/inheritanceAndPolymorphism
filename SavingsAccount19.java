c6ef6b06199787753222c3050fc678ffde253798
StefanyTodoroff <72222524+StefanyTodoroff@users.noreply.github.com>
2020-12-13 12:57:48 +0200

    Create SavingsAccount19.java

---
+++ SavingsAccount19.java
@@ -0,0 +1,19 @@
+package edu.smg;
+
+
+	public class SavingsAccount extends Account {
+		public SavingsAccount( int id, double balance){
+			super(id, balance);
+		}
+		
+		public void withdraw(double draw) {
+			if(balance >= draw ){
+			balance -= draw;
+			}
+		}
+		public String toString() {
+			return super.toString() + "Savings Account";
+		}
+		
+
+	}
